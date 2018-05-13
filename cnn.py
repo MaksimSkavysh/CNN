bd5daa0997fd3e095a93ab0572ffb0ae8c9c67ee
MaksimSkavysh <maksim.skavysh@gmail.com>
2018-05-13 14:21:42 +0300

    fix

--- cnn.py
+++ cnn.py
@@ -9,81 +9,89 @@ from tflearn.layers.estimator import regression
 from tflearn.data_utils import image_preloader
 from tflearn.initializations import variance_scaling
 
-ITERATION = 5
+ITERATION = 1
 
-FOLDER_TO_SAVE = './out/iteration' + str(ITERATION)
-FOLDER_TO_LOAD = './out/iteration' + str(ITERATION-1) + '/model/alex_model'
+def run_cnn(size):
 
-TRAIN_DATA = './train_data'
-VAL_DATA = './val_data'
+    print('\n\n\nstart with size: ' + str(size))
+    FOLDER_TO_SAVE = './out/alex_s' + str(size) + '_' + str(ITERATION)
+    # FOLDER_TO_LOAD = './out/iteration' + str(ITERATION - 1) + '/model/alex_model'
 
-X, Y = image_preloader(TRAIN_DATA,
-                       image_shape=(128, 128),
-                       mode='file',
-                       files_extension=['.png'])
+    TRAIN_DATA = './train_data'
+    VAL_DATA = './val_data'
 
-X_val, Y_val = image_preloader(VAL_DATA,
-                       image_shape=(128, 128),
-                       mode='file',
-                       files_extension=['.png'])
+    X, Y = image_preloader(TRAIN_DATA,
+                           image_shape=(size, size),
+                           mode='file',
+                           files_extension=['.png'])
+
+    X_val, Y_val = image_preloader(VAL_DATA,
+                                   image_shape=(size, size),
+                                   mode='file',
+                                   files_extension=['.png'])
+
+    print('Start building ...')
+    network = input_data(shape=[None, size, size, 3])
+    network = conv_2d(network,
+                      nb_filter=96,
+                      filter_size=11,
+                      strides=4,
+                      activation='relu',
+                      regularizer='L2',
+                      weight_decay=0.0005,
+                      bias_init='uniform',
+                      trainable=True,
+                      restore=True)
+    network = max_pool_2d(network, 3, strides=2)
+    network = local_response_normalization(network)
+    network = conv_2d(network, 256, 5, activation='relu')
+
+    network = max_pool_2d(network, 3, strides=2)
+    network = local_response_normalization(network)
+    network = conv_2d(network, 384, 3, activation='relu')
+    network = conv_2d(network, 384, 3, activation='relu')
+    network = conv_2d(network, 256, 3, activation='relu')
+    network = max_pool_2d(network, 3, strides=2)
+    network = local_response_normalization(network)
+    network = fully_connected(network, 4096, activation='relu')
+    network = dropout(network, 0.5)
+    network = fully_connected(network, 4096, activation='relu')
+    network = dropout(network, 0.5)
+    network = fully_connected(network, 2, activation='softmax')
+    network = regression(network,
+                         optimizer='momentum',
+                         loss='categorical_crossentropy',
+                         learning_rate=0.0005)
+
+    # Training
+    model = tflearn.DNN(network,
+                        checkpoint_path=FOLDER_TO_SAVE + '/checkpoints/',
+                        tensorboard_dir=FOLDER_TO_SAVE + '/logs/',
+                        best_checkpoint_path=FOLDER_TO_SAVE + '/best_checkpoint',
+                        max_checkpoints=3,
+                        best_val_accuracy=0.6,
+                        tensorboard_verbose=0)
+
+    # print('\nStart loading ' + FOLDER_TO_LOAD + ' ... ')
+    # model.load(FOLDER_TO_LOAD)
+
+    print('\nStart training ...')
+    model.fit(X,
+              Y,
+              n_epoch=20,
+              validation_set=(X_val, Y_val),
+              shuffle=True,
+              batch_size=128,
+              show_metric=True,
+              snapshot_epoch=True,
+              run_id='alex_TB5')
+
+    print('\nStart saving ...')
+    model.save(FOLDER_TO_SAVE + '/model/alex_model')
 
 
-print('Start building ...')
-network = input_data(shape=[None, 128, 128, 3])
-network = conv_2d(network,
-                  nb_filter=96,
-                  filter_size=11,
-                  strides=4,
-                  activation='relu',
-                  regularizer='L2',
-                  weight_decay=0.0005,
-                  bias_init='uniform',
-                  trainable=True,
-                  restore=True)
-network = max_pool_2d(network, 3, strides=2)
-network = local_response_normalization(network)
-network = conv_2d(network, 256, 5, activation='relu')
-
-network = max_pool_2d(network, 3, strides=2)
-network = local_response_normalization(network)
-network = conv_2d(network, 384, 3, activation='relu')
-network = conv_2d(network, 384, 3, activation='relu')
-network = conv_2d(network, 256, 3, activation='relu')
-network = max_pool_2d(network, 3, strides=2)
-network = local_response_normalization(network)
-network = fully_connected(network, 4096, activation='relu')
-network = dropout(network, 0.5)
-network = fully_connected(network, 4096, activation='relu')
-network = dropout(network, 0.5)
-network = fully_connected(network, 2, activation='softmax')
-network = regression(network,
-                     optimizer='momentum',
-                     loss='categorical_crossentropy',
-                     learning_rate=0.0005)
-
-# Training
-model = tflearn.DNN(network,
-                    checkpoint_path=FOLDER_TO_SAVE + '/checkpoints/',
-                    tensorboard_dir=FOLDER_TO_SAVE + '/logs/',
-                    best_checkpoint_path=FOLDER_TO_SAVE + '/best_checkpoint',
-                    max_checkpoints=3,
-                    best_val_accuracy=0.6,
-                    tensorboard_verbose=0)
-
-
-print('\nStart loading ' + FOLDER_TO_LOAD + ' ... ')
-model.load(FOLDER_TO_LOAD)
-
-print('\nStart training ...')
-model.fit(X,
-          Y,
-          n_epoch=100,
-          validation_set=(X_val, Y_val),
-          shuffle=True,
-          batch_size=128,
-          show_metric=True,
-          snapshot_epoch=True,
-          run_id='alex_TB5')
-
-print('\nStart saving ...')
-model.save(FOLDER_TO_SAVE + '/model/alex_model')
+run_cnn(96)
+run_cnn(64)
+run_cnn(32)
+run_cnn(160)
+run_cnn(256)
--- vgg_check.py
+++ vgg_check.py
@@ -6,13 +6,10 @@ from tflearn.layers.conv import conv_2d, max_pool_2d
 from tflearn.layers.estimator import regression
 from tflearn.data_utils import image_preloader
 
-
-print('\nVGG network with 128x128 images input\n')
-
 # Data loading and preprocessing
 ITERATION = 2
 
-FOLDER_TO_SAVE = './out/vgg_im128_i' + str(ITERATION)
+FOLDER_TO_SAVE = './out/vgg_i' + str(ITERATION)
 # FOLDER_TO_LOAD = './out/vgg_i' + str(ITERATION-1) + '/model/alex_model'
 # FOLDER_TO_LOAD = './out/vgg_i' + str(ITERATION) + '/best_checkpoint9255'
 FOLDER_TO_LOAD = './vgg_i2/best_checkpoint9255'
@@ -20,20 +17,9 @@ FOLDER_TO_LOAD = './vgg_i2/best_checkpoint9255'
 TRAIN_DATA = './train_data'
 VAL_DATA = './val_data'
 
-X, Y = image_preloader(TRAIN_DATA,
-                       image_shape=(128, 128),
-                       mode='file',
-                       files_extension=['.png'])
-
-X_val, Y_val = image_preloader(
-    VAL_DATA,
-    image_shape=(128, 128),
-    mode='file',
-    files_extension=['.png'],
-)
 
 # Building 'VGG Network'
-network = input_data(shape=[None, 128, 128, 3])
+network = input_data(shape=[None, 224, 224, 3])
 
 network = conv_2d(network, 64, 3, activation='relu')
 network = conv_2d(network, 64, 3, activation='relu')
@@ -80,18 +66,18 @@ model = tflearn.DNN(network,
 
 # # print('\nStart loading ' + FOLDER_TO_LOAD + ' ... ')
 # # model.load(FOLDER_TO_LOAD)
-#
-# print('\nStart loading ' + FOLDER_TO_LOAD + ' ... ')
-# model.load(FOLDER_TO_LOAD)
 
-print('\nStart training ...')
-model.fit(X, Y,
-          n_epoch=20,
-          validation_set=(X_val, Y_val),
-          shuffle=True,
-          show_metric=True,
-          batch_size=32,
-          snapshot_step=500,
-          snapshot_epoch=True,
-          run_id='vgg_TB5')
+print('\nStart loading ' + FOLDER_TO_LOAD + ' ... ')
+model.load(FOLDER_TO_LOAD)
+
+# print('\nStart training ...')
+# model.fit(X, Y,
+#           n_epoch=2,
+#           validation_set=(X_val, Y_val),
+#           shuffle=True,
+#           show_metric=True,
+#           batch_size=16,
+#           snapshot_step=500,
+#           snapshot_epoch=True,
+#           run_id='vgg_TB5')
 
