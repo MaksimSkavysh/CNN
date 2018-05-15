fdf0278a5b7f9ff25b1960d476d94fd093222aa1
MaksimSkavysh <maksim.skavysh@gmail.com>
2018-05-15 09:25:35 +0300

    fix

--- cnn.py
+++ cnn.py
@@ -2,7 +2,7 @@ from __future__ import division, print_function, absolute_import
 import sys
 
 
-def run_cnn(size):
+def img_size_run_cnn(size):
     # import numpy as np
     # from numpy import array
     import tflearn
@@ -92,8 +92,105 @@ def run_cnn(size):
     print('\nStart saving ...')
     model.save(FOLDER_TO_SAVE + '/model/model')
 
-
 # python3.6 ./cnn.py 32 &> cnn_32.txt; python3.6 ./cnn.py 64 &> cnn_64.txt; python3.6 ./cnn.py 128 &> cnn_128.txt; python3.6 ./cnn.py 160 &> cnn_160.txt; python3.6 ./cnn.py 256 &> cnn_256.txt
+
+
+def filter_size_run_cnn(filter_size):
+    # import numpy as np
+    # from numpy import array
+    image_size = 128
+    strides = 4
+    if filter_size < 9:
+        strides = 2
+    if filter_size < 5:
+        strides = 1
+    import tflearn
+    from tflearn.layers.core import input_data, dropout, fully_connected
+    from tflearn.layers.conv import conv_2d, max_pool_2d
+    from tflearn.layers.normalization import local_response_normalization
+    from tflearn.layers.estimator import regression
+    from tflearn.data_utils import image_preloader
+    from tflearn.initializations import variance_scaling
+
+    ITERATION = 1
+
+    print('\n\n\nstart with filter size: ' + str(filter_size) + '; and strides: ' + str(strides))
+    FOLDER_TO_SAVE = './out/alex_f' + str(filter_size) + '_' + str(ITERATION)
+    FOLDER_TO_LOAD = './out/alex_f' + str(image_size) + '_' + str(ITERATION-1) + '/model/alex_model'
+
+    TRAIN_DATA = './train_data'
+    VAL_DATA = './val_data'
+
+    X, Y = image_preloader(TRAIN_DATA,
+                           image_shape=(image_size, image_size),
+                           mode='file',
+                           files_extension=['.png'])
+
+    X_val, Y_val = image_preloader(VAL_DATA,
+                                   image_shape=(image_size, image_size),
+                                   mode='file',
+                                   files_extension=['.png'])
+
+    print('Start building ...')
+    network = input_data(shape=[None, image_size, image_size, 3])
+    network = conv_2d(network,
+                      nb_filter=96,
+                      filter_size=filter_size,
+                      strides=strides,
+                      activation='relu',
+                      regularizer='L2',
+                      weight_decay=0.0005,
+                      bias_init='uniform',
+                      trainable=True,
+                      restore=True)
+    network = max_pool_2d(network, 3, strides=2)
+    network = local_response_normalization(network)
+    network = conv_2d(network, 256, 5, activation='relu')
+
+    network = max_pool_2d(network, 3, strides=2)
+    network = local_response_normalization(network)
+    network = conv_2d(network, 384, 3, activation='relu')
+    network = conv_2d(network, 384, 3, activation='relu')
+    network = conv_2d(network, 256, 3, activation='relu')
+    network = max_pool_2d(network, 3, strides=2)
+    network = local_response_normalization(network)
+    network = fully_connected(network, 4096, activation='relu')
+    network = dropout(network, 0.5)
+    network = fully_connected(network, 4096, activation='relu')
+    network = dropout(network, 0.5)
+    network = fully_connected(network, 2, activation='softmax')
+    network = regression(network,
+                         optimizer='momentum',
+                         loss='categorical_crossentropy',
+                         learning_rate=0.0003)
+
+    # Training
+    model = tflearn.DNN(network,
+                        checkpoint_path=FOLDER_TO_SAVE + '/checkpoints/',
+                        tensorboard_dir=FOLDER_TO_SAVE + '/logs/',
+                        best_checkpoint_path=FOLDER_TO_SAVE + '/best_checkpoint',
+                        max_checkpoints=3,
+                        best_val_accuracy=0.6,
+                        tensorboard_verbose=0)
+
+    # print('\nStart loading ' + FOLDER_TO_LOAD + ' ... ')
+    # model.load(FOLDER_TO_LOAD)
+
+    print('\nStart training ...')
+    model.fit(X,
+              Y,
+              n_epoch=50,
+              validation_set=(X_val, Y_val),
+              shuffle=True,
+              batch_size=128,
+              show_metric=True,
+              snapshot_epoch=True,
+              run_id='alex_TB5')
+
+    print('\nStart saving ...')
+    model.save(FOLDER_TO_SAVE + '/model/model')
+
+# python3.6 ./cnn.py 3 &> cnn_filter_3.txt; python3.6 ./cnn.py 5 &> cnn_filter_5.txt; python3.6 ./cnn.py 7 &> cnn_filter_7.txt; python3.6 ./cnn.py 9 &> cnn_filter_9.txt; python3.6 ./cnn.py 11 &> cnn_filter_11.txt;
 
 
 def main():
@@ -101,12 +198,8 @@ def main():
     if len(sys.argv) > 1:
         size = int(sys.argv[1])
         print(size)
-        run_cnn(size)
-        # run_cnn(32)
-        # run_cnn(64)
-        # run_cnn(128)
-        # run_cnn(160)
-        # run_cnn(256)
-
+        filter_size_run_cnn(size)
 
 main()
+
+# filter_size_run_cnn(9)
